4b4910372f2e90dbdc18b413026b5e7d12017772
KeonU <anythingthannothing@gmail.com>
2024-07-25 12:59:50 +0900

    feat: add sidebar component

--- kiary-web-app/src/components/sidebar.tsx
+++ kiary-web-app/src/components/sidebar.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import Link from 'next/link';
 
-function Sidebar(props) {
-  return <nav className={'sidebar'}></nav>;
+function Sidebar() {
+  return (
+    <nav className={'sidebar'}>
+      <ul className="side-nav">
+        <li className="side-nav__item">
+          <Link href="#" className="side-nav__link">
+            <span>Todo</span>
+          </Link>
+          <Link href="#" className="side-nav__link">
+            <span>Album</span>
+          </Link>
+          <Link href="#" className="side-nav__link">
+            <span>Calendar</span>
+          </Link>
+        </li>
+      </ul>
+      <div className="legal">&copy; 2024 by kku. All rights reserved.</div>
+    </nav>
+  );
 }
 
 export default Sidebar;
